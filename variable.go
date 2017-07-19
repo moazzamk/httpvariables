1902e02831aedf02fc0177b2884a4df16a22258a
Moazzam Khan <moazzamk@gmail.com>
2017-07-18 21:12:49 -0500

    initial commit

---
+++ variable.go
@@ -0,0 +1,10 @@
+package variable
+
+
+import "text/template"
+// TODO: implement replace variables functions.
+// Variables are valid json, they can be either an array or object.
+
+func ReplaceVariable() {
+	
+}
